586d85aad76049311ea389df14d5d7994bb4664a
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2023-03-17 21:25:16 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

---
+++ my-folder/problems/maximum_subarray/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maks = nums[0]
+        total = 0
+
+        for tall in nums:
+            total = max(0, total)
+            total += tall
+            maks = max(maks, total)
+        return maks
+
+        
