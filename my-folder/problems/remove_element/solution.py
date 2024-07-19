bcac90ff4e4559e9031ea8f65d03b802567633da
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-19 15:21:55 +0000

    Sync LeetCode submission - Remove Element (python3)

---
+++ my-folder/problems/remove_element/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        indeces = []
+        for i, elem in enumerate(nums):
+            if elem == val:
+                indeces.append(i)
+        for idx in reversed(indeces):
+            nums.pop(idx)
+        return len(nums)
