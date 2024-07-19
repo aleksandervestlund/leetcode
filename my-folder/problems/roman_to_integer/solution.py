5db800cb3ed12b34f96ed5cc18798bdb0337a4b0
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-19 12:02:18 +0000

    Sync LeetCode submission - Roman to Integer (python3)

---
+++ my-folder/problems/roman_to_integer/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbols = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        total = 0
+        idx = 0
+        
+        while idx < len(s):
+            sub = symbols.get(s[idx : idx + 2])
+            if sub is not None:
+                total += sub
+                idx += 2
+            else:
+                total += symbols[s[idx]]
+                idx += 1
+        
+        return total
+
+
+        
