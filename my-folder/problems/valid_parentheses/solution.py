d7d3e255948421029655338b86ca0a77092d1dcb
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-19 13:04:13 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

---
+++ my-folder/problems/valid_parentheses/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        complement = {"{": "}", "(": ")", "[": "]"}
+        queue = []
+
+        for char in s:
+            end = complement.get(char)
+            if end is None:
+                if not (queue and char == queue.pop()):
+                    return False
+            else:
+                queue.append(end)
+        
+        return not queue
+                
