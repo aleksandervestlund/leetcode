d1c526de9c95917504d811f21265443869a4014f
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-19 18:25:57 +0000

    Sync LeetCode submission - Search a 2D Matrix (python3)

---
+++ my-folder/problems/search_a_2d_matrix/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def bisect(a: list[int], p: int, r: int, v: int) -> int | None:
+            if p > r:
+                return None
+            q = (p + r) // 2
+            if v == a[q]:
+                return q
+            if v < a[q]:
+                return bisect(a, p, q - 1, v)
+            return bisect(a, q + 1, r, v)
+        
+        a = list(itertools.chain.from_iterable(matrix))
+        return bisect(a, 0, len(a) - 1, target) is not None
