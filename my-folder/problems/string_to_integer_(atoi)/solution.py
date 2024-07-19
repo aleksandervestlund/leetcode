46fa20e2a839f386308152bae6fac8c89ca47014
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-19 11:08:15 +0000

    Sync LeetCode submission - String to Integer (atoi) (python3)

---
+++ my-folder/problems/string_to_integer_(atoi)/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip(" ")
+        if not s:
+            return 0
+        sign = ""
+        for char in s:
+            if (not sign and char in {"+", "-"}) or char.isdigit():
+                sign += char
+            else:
+                break
+        if sign in {"+", "-", ""}:
+            return 0
+        sign = max(-2**31, int(sign))
+        return min(2**31-1, sign)
