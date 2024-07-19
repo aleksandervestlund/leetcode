164f3ff94d34a627cfcc38efdf8d7dc15d92e58e
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-19 18:09:48 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

---
+++ my-folder/problems/climbing_stairs/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self) -> None:
+        self.fibs = [0, 1]
+
+    def climbStairs(self, n: int) -> int:
+        def fib(n):
+            if len(self.fibs) >= n:
+                return self.fibs[n-1]
+            if n > 2:
+                for i in range(2, n):
+                    self.fibs.append(self.fibs[i - 1] + self.fibs[i - 2])
+            return self.fibs[n-1]
+        return fib(n + 2)
