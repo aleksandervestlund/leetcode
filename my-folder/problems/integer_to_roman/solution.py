7efc4b89b7e25a4e559120ba65a265fb2b273687
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-19 11:50:57 +0000

    Sync LeetCode submission - Integer to Roman (python3)

---
+++ my-folder/problems/integer_to_roman/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbols = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+        leading = 10**len(str(num))
+        s = ""
+
+        while num:
+            leading //= 10
+            digit, num = divmod(num, leading)
+            if digit in {4, 9}:
+                s += symbols[leading] + symbols[(digit + 1) * leading]
+                continue
+            if digit > 4:
+                s += symbols[5 * leading]
+                digit -= 5
+            if digit == 0:
+                continue
+            s += digit * symbols[leading]
+            
+
+        return s
