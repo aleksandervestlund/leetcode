7c8723a9a8e4bf23190e50767cde2fe909950aac
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-20 14:43:14 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

---
+++ my-folder/problems/pascal's_triangle/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows < 3:
+            return [[1] * i for i in range(1, numRows + 1)]
+        pascal = [[1], [1, 1]]
+        for i in range(2, numRows):
+            row = [1, 1]
+            for i in range(len(pascal[-1]) - 1):
+                row.insert(1, sum(pascal[-1][i : i + 2]))
+            pascal.append(row)
+        return pascal
