ce8d2c32f3c0e05e7d929799e32ce93d6716f852
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-20 13:31:42 +0000

    Sync LeetCode submission - Symmetric Tree (python3)

---
+++ my-folder/problems/symmetric_tree/solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+            if p is None and q is None:
+                return True
+            if p is None or q is None or p.val != q.val:
+                return False
+            return isSameTree(p.left, q.right) and isSameTree(p.right, q.left)
+        return isSameTree(root.left, root.right)
