cb13e2c4a12c2c79b6d94fefd6e2a7398408c9a7
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-20 12:22:59 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

---
+++ my-folder/problems/merge_sorted_array/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort()
+        # nums2.append(float("inf"))
+        # nums3 = nums1[:m] + [float("inf")]
+        # left = nums3.pop(0)
+        # right = nums2.pop(0)
+
+        # for k in range(m + n):
+        #     if left <= right:
+        #         nums1[k] = left
+        #         left = nums3.pop(0)
+        #     else:
+        #         nums1[k] = right
+        #         right = nums2.pop(0)
