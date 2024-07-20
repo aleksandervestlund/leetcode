e9f67cac0193a1b791407e856b442938a8097f02
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-20 23:28:13 +0000

    Sync LeetCode submission - Maximum Gap (python3)

---
+++ my-folder/problems/maximum_gap/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        nums = sorted(set(nums))
+        n = len(nums)
+        if n < 2:
+            return 0
+        return max(nums[i + 1] - nums[i] for i in range(n - 1))
