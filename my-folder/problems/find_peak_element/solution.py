e3986089a01f22981c240ca728969bbf06e369bf
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-20 23:23:14 +0000

    Sync LeetCode submission - Find Peak Element (python3)

---
+++ my-folder/problems/find_peak_element/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        nums.insert(0, float("-inf"))
+        nums.append(float("-inf"))
+        for i in range(1, len(nums) - 1):
+            if nums[i - 1] < nums[i] > nums[i + 1]:
+                return i - 1
+    
