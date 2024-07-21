d4d98fe244c5e34193f7ad3b5a726dc2316e2d89
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-21 10:38:21 +0000

    Sync LeetCode submission - Repeated DNA Sequences (python3)

---
+++ my-folder/problems/repeated_dna_sequences/solution.py
@@ -0,0 +1,3 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        return [key for key, value in Counter(s[i : i + 10] for i in range(len(s) - 9)).items() if value > 1]
