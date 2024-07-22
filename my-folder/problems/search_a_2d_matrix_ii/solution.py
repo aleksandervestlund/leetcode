fd1954177981647420870fe8c223db2a71709501
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-22 12:29:46 +0000

    Sync LeetCode submission - Search a 2D Matrix II (python3)

---
+++ my-folder/problems/search_a_2d_matrix_ii/solution.py
@@ -0,0 +1,18 @@
+def bisect(a: list[int], p: int, r: int, v: int) -> int | None:
+    if p > r:
+        return False
+    q = (p + r) // 2
+    if v == a[q]:
+        return True
+    if v < a[q]:
+        return bisect(a, p, q - 1, v)
+    return bisect(a, q + 1, r, v)
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix[0])
+        return any(
+            bisect(row, 0, n - 1, target)
+            for row in takewhile(lambda x: x[0] <= target, matrix)
+        )
