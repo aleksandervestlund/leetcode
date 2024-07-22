0c5dd7d6cecdcaaadc1a6553d23d1e7318f1d831
Aleksander Thornes Vestlund <144712078+aleksandervestlund@users.noreply.github.com>
2024-07-22 11:51:26 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

---
+++ my-folder/problems/product_of_array_except_self/solution.py
@@ -0,0 +1,16 @@
+from math import prod
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        zeroes = nums.count(0)
+
+        if zeroes > 1:
+            return [0] * n
+
+        product = prod(elem for elem in nums if elem != 0)
+
+        if zeroes == 1:
+            return [0 if elem else product for elem in nums]
+        return [product // nums[i] for i in range(n)]
